f8f34882188c9e5f029da288926dce359638b822
lengqie <lengqie@foxmail.com>
2022-02-05 16:10:05 +0800

    use vue-manage-system

--- web/src/main.js
+++ web/src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-
-// import router from "./router/index"
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap/dist/js/bootstrap.min";
-
-
+import router from './router'
+import store from './store'
+import installElementPlus from './plugins/element'
+import './assets/css/icon.css'
 const app = createApp(App)
-// app.use(router)
-app.mount('#app')
+installElementPlus(app)
+app
+    .use(store)
+    .use(router)
+    .mount('#app')
